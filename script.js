8433e35a93a2ed9f0c14c41a28fa7f6a42057a23
Raul Cardoso <byruzie@gmail.com>
2025-06-04 20:27:26 -0300

    máscara para valor monetário

---
+++ script.js
@@ -0,0 +1,8 @@
+// máscara de valor monetário
+$(document).ready(function(){
+  $('#from').mask('000.000,00', {reverse: true});
+});
+
+$(document).ready(function(){
+  $('#for').mask('000.000,00', {reverse: true});
+});
