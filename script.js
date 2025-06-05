36476c661ac6b3847018f2c18729a350b93a87fb
Raul Cardoso <byruzie@gmail.com>
2025-06-04 21:58:06 -0300

    consumindo api

--- script.js
+++ script.js
@@ -7,4 +7,14 @@ $(document).ready(function () {
   $("#for").mask("000.000,00", { reverse: true });
 });
 
-// 
+// api com taxas de câmbio
+// 6971c71c0b89341728d6c0f5
+
+fetch("https://v6.exchangerate-api.com/v6/6971c71c0b89341728d6c0f5/latest/USD")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Erro ao obter dados:", error);
+  });
