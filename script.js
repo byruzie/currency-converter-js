bfbf427c30775d418115489fef5f5f77e99af315
Raul Cardoso <byruzie@gmail.com>
2025-06-04 21:22:23 -0300

    criando select com opções de moedas no html

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // máscara de valor monetário
-$(document).ready(function(){
-  $('#from').mask('000.000,00', {reverse: true});
+$(document).ready(function () {
+  $("#from").mask("000.000,00", { reverse: true });
 });
 
-$(document).ready(function(){
-  $('#for').mask('000.000,00', {reverse: true});
+$(document).ready(function () {
+  $("#for").mask("000.000,00", { reverse: true });
 });
+
+// 
