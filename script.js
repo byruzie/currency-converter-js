987847b56713050c1bb8a80d07e9cb93ddccdce3
Raul Cardoso <byruzie@gmail.com>
2025-06-05 15:04:22 -0300

    criando correspondência de valores entre os inputs

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-  $("#for").mask("000.000,00", { reverse: true });
+  $("#to").mask("000.000,00", { reverse: true });
 });
 
 // api com taxas de câmbio
@@ -25,7 +25,6 @@ fetch("https://v6.exchangerate-api.com/v6/6971c71c0b89341728d6c0f5/latest/USD")
     });
 
     Object.entries(conversionRates).forEach(([moeda, taxa]) => {
-      // transforma em array
       const option = document.createElement("option"); // cria options no select de moedas
       option.value = moeda;
       option.textContent = moeda;
@@ -36,9 +35,30 @@ fetch("https://v6.exchangerate-api.com/v6/6971c71c0b89341728d6c0f5/latest/USD")
     optionFromElement.setAttribute("selected", "selected");
     const optionToElement = document.querySelector("#select-to option[value='BRL']"); // seleciona BRL como padrão do select-to
     optionToElement.setAttribute("selected", "selected");
-
-    console.log(data);
   })
   .catch((error) => {
     console.error("Erro ao obter dados:", error);
   });
+
+// input from e to
+const inputFromEl = document.getElementById("from");
+const inputToEl = document.getElementById("to");
+const inputFromDiv = document.getElementById("from-div");
+const inputToDiv = document.getElementById("to-div");
+
+function activateInput(input1, input2) {
+  input1.disabled = false;
+  input1.focus();
+  input2.disabled = true;
+}
+
+inputToDiv.addEventListener("click", () => activateInput(inputToEl, inputFromEl));
+inputFromDiv.addEventListener("click", () => activateInput(inputFromEl, inputToEl));
+
+inputFromEl.addEventListener("input", function() {
+  inputToEl.value = this.value;
+});
+
+inputToEl.addEventListener("input", function() {
+  inputFromEl.value = this.value;
+});
