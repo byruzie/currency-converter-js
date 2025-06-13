1426e9bee83c18d7f91775883f07ee9ff7895558
Raul Cardoso <byruzie@gmail.com>
2025-06-13 17:41:43 -0300

    adicionar modo de teste com dados mockados

--- script.js
+++ script.js
@@ -7,117 +7,135 @@ $(document).ready(function () {
   $("#to").mask("000.000,00", { reverse: true });
 });
 
-// api com taxas de câmbio
+// FLAG DE TESTE
+const MODO_TESTE = true;
+
+// MOCK DE DADOS
+const mockData = {
+  conversion_rates: {
+    USD: 1,
+    BRL: 5.25,
+    EUR: 0.93,
+    JPY: 156.78
+  }
+};
+
 const selectFromEl = document.getElementById("select-from");
 const selectToEl = document.getElementById("select-to");
 
-fetch("https://v6.exchangerate-api.com/v6/6971c71c0b89341728d6c0f5/latest/USD")
+function iniciateConverter(data) {
+  const conversionRates = data.conversion_rates; // objeto
+  const moedas = Object.keys(conversionRates); // array
+
+  // cria options no select de moedas
+  function setOptions(selectEl) {
+    moedas.forEach((moeda) => {
+      const option = document.createElement("option");
+      option.value = moeda;
+      option.textContent = moeda;
+      selectEl.appendChild(option);
+    });
+  }
+  setOptions(selectFromEl);
+  setOptions(selectToEl);
+
+  // seleciona USD como padrão do select-from
+  document.querySelector("#select-from").value = "USD";
+  // seleciona BRL como padrão do select-to
+  document.querySelector("#select-to").value = "BRL";
+
+// inverter as moedas
+  const iconAlt = document.getElementById("alt");
+  iconAlt.addEventListener("click", () => {
+    // inverte as moedas
+    const optionFromValue = document.querySelector("#select-from").value;
+    const optionToValue = document.querySelector("#select-to").value;
+    document.querySelector("#select-from").value = `${optionToValue}`;
+    document.querySelector("#select-to").value = `${optionFromValue}`;
+
+    // se tiver valor, inverte o valor
+    if (inputFromEl.value) {
+      const valueFrom = inputFromEl.value;
+      const valueTo = inputToEl.value;
+      inputFromEl.value = valueTo;
+      inputToEl.value = valueFrom;
+    }
+  });
+
+  // função de conversão
+  function converter() {
+    const valorFrom = parseFloat(
+      inputFromEl.value.replace(".", "").replace(",", ".")
+    );
+    const valorTo = parseFloat(
+      inputToEl.value.replace(".", "").replace(",", ".")
+    );
+    const taxaDe = conversionRates[selectFromEl.value];
+    const taxaPara = conversionRates[selectToEl.value];
+
+    if (!inputFromEl.disabled) {
+      if (isNaN(valorFrom)) {
+        inputToEl.value = "";
+        return;
+      }
+      const convertido = (valorFrom / taxaDe) * taxaPara;
+      inputToEl.value = convertido.toFixed(2).replace(".", ",");
+    } else if (!inputToEl.disabled) {
+      if (isNaN(valorTo)) {
+        inputFromEl.value = "";
+        return;
+      }
+      const convertido = (valorTo / taxaPara) * taxaDe;
+      inputFromEl.value = convertido.toFixed(2).replace(".", ",");
+    }
+  }
+
+  // define 1 como valor padrão
+  (function defaultValue() {
+    inputFromEl.value = "1,00";
+    converter();
+  })();
+
+  // caixa de exemplo por unidade
+  function unitValue() {
+    const h1From = document.getElementById("h1-from");
+    h1From.textContent = `1 ${selectFromEl.value} =`;
+
+    const h1To = document.getElementById("h1-to");
+    const taxaPara = conversionRates[selectToEl.value];
+    const convertido = 1 * taxaPara;
+    h1To.textContent = `${convertido.toFixed(2).replace(".", ",")} ${selectToEl.value} `;
+  }
+
+  unitValue();
+
+  // converte valor do input de from para to
+  inputFromEl.addEventListener("input", () => converter());
+  // converte valor do input de to para from
+  inputToEl.addEventListener("input", () => converter());
+
+  // converte ao trocar moeda selecionada
+  selectFromEl.addEventListener("change", () => {
+    converter();
+    unitValue()
+  });
+
+  selectToEl.addEventListener("change", () => {
+    converter();
+  });
+}
+
+// api com taxas de câmbio
+if(MODO_TESTE) {
+  iniciateConverter(mockData);
+} else {
+  fetch("https://v6.exchangerate-api.com/v6/6971c71c0b89341728d6c0f5/latest/USD")
   .then((response) => response.json())
-  .then((data) => {
-    const conversionRates = data.conversion_rates; // objeto
-    const taxas = Object.values(conversionRates); // array
-    const moedas = Object.keys(conversionRates); // array
-
-    // cria options no select de moedas
-    function setOptions(selectEl) {
-      moedas.forEach((moeda) => {
-        const option = document.createElement("option");
-        option.value = moeda;
-        option.textContent = moeda;
-        selectEl.appendChild(option);
-      });
-    }
-    setOptions(selectFromEl);
-    setOptions(selectToEl);
-
-    // seleciona USD como padrão do select-from
-    document.querySelector("#select-from").value = "USD";
-    // seleciona BRL como padrão do select-to
-    document.querySelector("#select-to").value = "BRL";
-
-    // inverter as moedas
-    const iconAlt = document.getElementById("alt");
-    iconAlt.addEventListener("click", () => {
-      // inverte as moedas
-      const optionFromValue = document.querySelector("#select-from").value;
-      const optionToValue = document.querySelector("#select-to").value;
-      document.querySelector("#select-from").value = `${optionToValue}`;
-      document.querySelector("#select-to").value = `${optionFromValue}`;
-
-      // se tiver valor, inverte o valor
-      if (inputFromEl.value) {
-        const valueFrom = inputFromEl.value;
-        const valueTo = inputToEl.value;
-        inputFromEl.value = valueTo;
-        inputToEl.value = valueFrom;
-      }
-    });
-
-    // função de conversão
-    function converter() {
-      const valorFrom = parseFloat(
-        inputFromEl.value.replace(".", "").replace(",", ".")
-      );
-      const valorTo = parseFloat(
-        inputToEl.value.replace(".", "").replace(",", ".")
-      );
-      const taxaDe = conversionRates[selectFromEl.value];
-      const taxaPara = conversionRates[selectToEl.value];
-
-      if (!inputFromEl.disabled) {
-        if (isNaN(valorFrom)) {
-          inputToEl.value = "";
-          return;
-        }
-        const convertido = (valorFrom / taxaDe) * taxaPara;
-        inputToEl.value = convertido.toFixed(2).replace(".", ",");
-      } else if (!inputToEl.disabled) {
-        if (isNaN(valorTo)) {
-          inputFromEl.value = "";
-          return;
-        }
-        const convertido = (valorTo / taxaPara) * taxaDe;
-        inputFromEl.value = convertido.toFixed(2).replace(".", ",");
-      }
-    }
-
-    // define 1 como valor padrão
-    (function defaultValue() {
-      inputFromEl.value = "1,00";
-      converter();
-    })();
-
-    // caixa de exemplo por unidade
-    function unitValue() {
-      const h1From = document.getElementById("h1-from");
-      h1From.textContent = `1 ${selectFromEl.value} =`;
-
-      const h1To = document.getElementById("h1-to");
-      const taxaPara = conversionRates[selectToEl.value];
-      const convertido = 1 * taxaPara;
-      h1To.textContent = `${convertido.toFixed(2).replace(".", ",")} ${selectToEl.value} `;
-    }
-
-    unitValue();
-
-    // converte valor do input de from para to
-    inputFromEl.addEventListener("input", () => converter());
-    // converte valor do input de to para from
-    inputToEl.addEventListener("input", () => converter());
-
-    // converte ao trocar moeda selecionada
-    selectFromEl.addEventListener("change", () => {
-      converter();
-      unitValue()
-    });
-
-    selectToEl.addEventListener("change", () => {
-      converter();
-    });
-  })
+  .then((data) => iniciateConverter(data))
   .catch((error) => {
     console.error("Erro ao obter dados:", error);
   });
+}
 
 // input from e to
 const inputFromEl = document.getElementById("from");
